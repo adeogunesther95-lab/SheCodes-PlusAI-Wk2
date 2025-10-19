b64fbe499868535af1ead9d5ba8ba44f62e6bc6d
adeogunesther95-lab <adeogunesther95@gmail.com>
2025-10-19 02:56:01 +0100

    added typewriter plugin and basic JS to form

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-text", {
+    strings: "",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
